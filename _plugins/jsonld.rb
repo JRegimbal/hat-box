8f74ee0d9de1755b6ec6b249ac27455019ecffc1
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-11-27 14:12:42 -0500

    New taxonomy, updated script
    Can be opened, edited using Protege

--- _plugins/jsonld.rb
+++ _plugins/jsonld.rb
@@ -10,29 +10,65 @@ module TestPlugin
     def generate(site)
       g = Git.open(site.source)
       site.collections['tools'].docs.each do |tool|
+        if tool.data["title"] == "Tools"
+          next
+        end
         lc = g.log(1).object(tool.path).first
-        lcdate = lc.date.to_s
+        #lcdate = lc.date.to_s
         lastcommit = lc.sha.to_s
         relsource = Pathname.new(tool.path).relative_path_from(Pathname.new(site.source)).to_s
         path = tool.destination( site.dest )
-        path['.html'] = '.json'
+        path['.html'] = '.jsonld'
         relpath = Pathname.new(path).relative_path_from(Pathname.new(site.dest)).to_s
         FileUtils.mkdir_p(File.dirname(path))
         File.open(path, "w") do |f|
           test = {}
-          test["@context"] = "http://schema.org/"
-          test["@type"] = "CreativeWork"
-          test["@id"] = site.config["url"] + site.baseurl + "/" + relpath
-          test["name"] = tool.data["title"]
-          test["datePublished"] = tool.data["year"]
-          test["availableOnDevice"] = tool.data["hardware_abstraction"]
-          test["sdPublisher"] = {
-            :@type => "Organization",
-            :name => "Various Project Contributors",
-            :url => site.config["gh_edit_repository"] + "/blame/" + lastcommit + "/" + relsource
+          test["@context"] = {
+            :hat => "http:/cim.mcgill.ca/~juliette/authoring/taxonomy.jsonld#",
+            :dcterms => "http://purl.org/dc/terms/"
           }
-          test["sdLicense"] = "https://creativecommons.org/licenses/by-sa/4.0/"
-          test["sdDatePublished"] = lcdate
+          test["@type"] = "hat:AuthoringTool"
+          thisId = site.config["url"] + site.baseurl + "/" + relpath
+          test["@id"] = thisId
+          test["dcterms:title"] = tool.data["title"]
+          test["hat:Year"] = tool.data["year"]
+          if tool.data["availability"] == "Proprietary" or tool.data["availability"] == "Unavailable"
+            test["hat:Availability"] = { :@id => "hat:" + tool.data["availability"] }
+          elsif tool.data["availability"] == "Source Available, Ambiguous License"
+            test["hat:Availability"] = { :@id => "hat:SAAL" }
+          elsif tool.data["availability"].include? "Open Source"
+            tmp = tool.data["availability"].split("(")[1]
+            tmp = tmp.split(")")[0]
+            test["hat:Availability"] = {
+              :@id => thisId + "#Availability",
+              :@type => "hat:OpenSource",
+              :@value => tmp
+            }
+          else
+            puts tool.data["availability"]
+          end
+          test["hat:HapticCategory"] = tool.data["haptic_category"].map {|val|
+            val.gsub! " ", ""
+            { :@id => "hat:" + val }
+          }
+          test["hat:DrivingFeature"] = tool.data["driving_feature"].map {|val| { :@id => "hat:" + val }}
+          test["hat:EffectLocalization"] = { :@id => "hat:" + tool.data["effect_localization"] }
+          test["hat:DesignApproaches"] = tool.data["design_approaches"].map {|val|
+            val.sub! "DPC", "DirectParametricControl"
+            { :@id => "hat:" + val }
+          }
+          if tool.data["interaction_metaphors"].length > 0 and not tool.data["interaction_metaphors"].include?("None")
+            test["hat:InteractionMetaphors"] = tool.data["interaction_metaphors"].map {|val|
+              val.gsub! " ", ""
+              { :@id => "hat:" + val}
+            }
+          end
+          if tool.data["media_support"].length > 0 and not tool.data["media_support"].include?("None")
+            test["hat:MediaSupport"] = tool.data["media_support"].map {|val| { :@id => "hat:" + val}}
+          end
+          test["dcterms:contributor"] = site.config["gh_edit_repository"] + "/blame/" + lastcommit + "/" + relsource
+          test["dcterms:license"] = "https://creativecommons.org/licenses/by-sa/4.0/"
+          #test["sdDatePublished"] = lcdate
 
           f.write(test.to_json)
         end
